41f20400b99c6cbb79ec98ae3475fbe7bd5ba6f6
ArayCHN <1172094131@qq.com>
2018-04-21 19:57:51 +0800

    added systick and encoder mode for velocity calculation

--- USER/motor_test.cpp
+++ USER/motor_test.cpp
@@ -9,17 +9,11 @@ void TIMx_PWMInit(uint16_t prescaler, uint16_t period, uint16_t pulse)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); // GPIO clock
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_speed_50MHz;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
     /* GPIO settings above */
     /* TIMx clock settings below */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM5, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB2Periph_TIM5, ENABLE);
     TIM_TimeBaseInitStructure.TIM_Prescaler = prescaler;
     TIM_TimeBaseInitStructure.TIM_Period = period;
     TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
@@ -45,12 +39,81 @@ void TIMx_PWMInit(uint16_t prescaler, uint16_t period, uint16_t pulse)
     return;
 }
 
+#define encoder_counter_reload 8000 // rotate each round, encoder around 200
+void EncodeInit(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure; // PB6, PB7
+    TIM_TimeBaseInitTypeDef  TIM4_TimeBaseStructure; // TIM4 CH1, 2 - PB6, 7 - Encoder_R1
+    TIM_ICInitTypeDef TIM4_ICInitStructure;
+
+    /* TIM4 Clock && GPIO enable */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    /* Configure PB6, 7 */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;       
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* Configure TIM4 */
+    TIM4_TimeBaseStructure.TIM_Prescaler = 0; // TIM clock = 72MHz        
+    TIM4_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM4_TimeBaseStructure.TIM_Period = encoder_counter_reload - 1; // what value?
+    TIM4_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM4_TimeBaseStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM4, &TIM4_TimeBaseStructure);
+
+    TIM_EncoderInterfaceConfig(TIM4, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising,
+                               TIM_ICPolarity_Rising); // 配置编码器模式，4倍频??? 需要测试一下！是不是要改成双向计数！
+
+    TIM_ICStructInit(&TIM4_ICInitStructure);
+    TIM4_ICInitStructure.TIM_ICFilter = 6; // filter, choose what value?
+    TIM_ICInit(TIM4, &TIM4_ICInitStructure);
+
+    TIM_ClearFlag(TIM4, TIM_FLAG_Update);
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+
+    TIM_SetCounter(TIM4, 0); // TIM4 -> CNT = 0
+    TIM_Cmd(TIM4, ENABLE);
+}
+
+static u16 last_cnt, counts, v;
+inline int get_encoder_velocity()
+{
+    counts = TIM -> CNT - last_cnt;
+    if (counts < - encoder_counter_reload / 2) // counter overflowed
+        counts = encoder_counter_reload + v;
+    last_cnt = TIM4 -> CNT;
+    return counts;
+}
+
+#define spokes_num 11
+#define reduction_ratio 19.8
+#define sysTick_period 10 // 10ms
+
+extern "C" void SysTick_Handler() // does "void" have to be written within ()?
+{
+    v = get_encoder_velocity() / spokes_num / reduction_ratio / sysTick_period; // 把除法拆开写
+    // v应该是小数，怎么用定点小数表示？
+    return;
+}
+
+void systickInit()
+{
+    SysTick_Config(SystemCoreClock / 1000 * sysTick_period - 1); // SystemCoreClock == 72MHz
+    return;
+}
+
+// 数脉冲宽度需要取五个或多个（维护队列）做平均滤波！
+
 int main()
 {
     uint16_t prescaler = 72 - 1; // 0~65535
     uint16_t period = 49;
     uint16_t pulse = 30;
-    TIMx_PWMInit(prescaler, period, pulse); // set GPIO
+    TIMx_PWMInit(prescaler, period, pulse); // set timer for motor
+    systickInit();
     while (1);
 	return 0;
 }
--- USER/stm32f10x_it.c
+++ USER/stm32f10x_it.c
@@ -79,6 +79,7 @@ void SysTick_Handler(void)
 {
 }
 
+
 /******************************************************************************/
 /*                 STM32F10x Peripherals Interrupt Handlers                   */
 /*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
