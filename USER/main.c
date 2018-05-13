5c014a672bafd37a9536bf6925de24873f636f24
ArayCHN <1172094131@qq.com>
2018-05-13 09:57:31 +0800

    test motor pwm control

--- USER/main.c
+++ USER/main.c
@@ -3,6 +3,7 @@
 #include "macros.h"
 
 void TIMx_PWMInit(uint16_t prescaler, uint16_t period, uint16_t pulse);
+void motor_pid_controller(int, int, int);
 
 void EncodeInit(void);
 
@@ -14,8 +15,7 @@ void TIM8_Cap_Init(void); // init for time interval method; need to change to TI
 // vars for time interval method
 int cnt_ch3 = 0, prev_cnt_ch3 = -1, cnt_ch4 = 0, prev_cnt_ch4 = -1, 
 	  delta_t3 = 0, delta_t4 = 0, cycles3 = 0, cycles4 = 0; // prev_cnt init -1 to prevent den==0
-int v3, v4;
-u32  num, den;
+int v3, v4, num, den;
 // cycles: the cycles of reloading in TIM. cuz there might have been multiple cycles between two interrupts!
 
 u8 obstacle_mode_flag = 0; // for ultrasonic, record if it is now in obstacle mode!
@@ -26,12 +26,14 @@ void Ultrasonic_Trig(void);
 
 // below: systick, whose exception mechanism is shared among several devices
 static u8 ultra_cnt; // only visible in main.c
+int omega, v_target;
 extern void SysTick_Handler()
 {
     // v = get_encoder_counts() * wheel_perimeter * 1000 / (spokes_num * reduction_ratio * sysTick_period * 4); // update velocity
     // v in mm/s; all vals must be signed int32 so that the multiplication doesn't overflow & there are pos & neg
+    omega = get_encoder_counts(); // n circles' 50ms
     int den = spokes_num * reduction_ratio * sysTick_period * 4; // divide encoder counter by 4
-    int num = get_encoder_counts() * wheel_perimeter * 1000 * 10; // *10 cuz reduction ratio is 21.3 instead of 213
+    int num = omega * wheel_perimeter * 1000 * 10; // *10 cuz reduction ratio is 21.3 instead of 213
     v = num / den;
 
     ultra_cnt = 1 - ultra_cnt;
@@ -49,6 +51,7 @@ extern void SysTick_Handler()
     //         else
     //            printf("left!\n");
     //     }
+    motor_pid_controller(1, 1, 1); // kp, ki, kd
     return;
 }
 
@@ -59,12 +62,17 @@ void systickInit()
     return;
 }
 
+void controller()
+{
+    v_target = 60;
+}
+
 int main()
 {
-    u16 prescaler = 72 - 1, period = 49, pulse = 30; // 0~65535
+    u16 prescaler = 72 - 1, motor_pwm_period = 49, pulse = 30; // 0~65535
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); // priority group config, 2 bits preemption, 2 bits sub
     // priority group config must be before anything!
-    TIMx_PWMInit(prescaler, period, pulse); // set timer for motor PWM
+    TIMx_PWMInit(prescaler, pulse); // set timer for motor PWM
     EncodeInit(); // use encoder mode for motor
     systickInit(); // encoder mode interupt (in fact, exception)
     TIM8_Cap_Init(); // init: motor velocity calculation with time interval
@@ -75,6 +83,7 @@ int main()
 
     while (1)
     {
+        controller(); // should be put on a time basis instead of always running!
         if (obstacle_mode_flag == NONE_OBSTACLE)
         {
             //printf("none!\n");
--- USER/motor_pwm.c
+++ USER/motor_pwm.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 #include "macros.h"
 
-void TIMx_PWMInit(uint16_t prescaler, uint16_t period, uint16_t pulse)
+static TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure; // only visible in this file
+static TIM_OCInitTypeDef TimOCInitStructure;
+extern u16 motor_pwm_period; // defined in main()
+static int err, err1, err2, delta_pulse;
+
+void TIMx_PWMInit(uint16_t prescaler, uint16_t pulse)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
-    TIM_OCInitTypeDef TimOCInitStructure;
+    // TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+    // TIM_OCInitTypeDef TimOCInitStructure;
     /* GPIO settings below */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); // GPIO clock
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
@@ -17,7 +22,7 @@ void TIMx_PWMInit(uint16_t prescaler, uint16_t period, uint16_t pulse)
     /* TIMx clock settings below */
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
     TIM_TimeBaseInitStructure.TIM_Prescaler = prescaler;
-    TIM_TimeBaseInitStructure.TIM_Period = period;
+    TIM_TimeBaseInitStructure.TIM_Period = motor_pwm_period;
     TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
     TIM_TimeBaseInit(TIM5, &TIM_TimeBaseInitStructure);
     /* TIMx clock settings above */
@@ -38,5 +43,55 @@ void TIMx_PWMInit(uint16_t prescaler, uint16_t period, uint16_t pulse)
     TIM_Cmd(TIM5, ENABLE);
     TIM_CtrlPWMOutputs(TIM5, ENABLE);
     /* PWM config above */
+    // config direction control output to L298N
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 
+                                | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    return;
+}
+
+void motor_pid_controller(int kp, int ki, int kd)
+{
+    // int R, delta_pulse;
+    // extern int R_target; // R_target is given by the general controller, global var
+    // R = W * (vr1 + vr2 + vl1 + vl2) / (vr1 - vl1 + vr2 - vl2) / 2; // is prevision good?
+    // err = R - R_target;
+    extern int v_target, v;
+    err = v_target - v;
+    delta_pulse = kp * (err - err1) + ki * err + kd * (err - 2 * err1 + err2);
+    err2 = err1;
+    err1 = err;
+    pulse += delta_pulse;
+    if (pulse < 0)
+    {
+        GPIO_SetBits(GPIO_D, GPIO_Pin_0);
+        GPIO_ResetBits(GPIO_D, GPIO_Pin_0);
+        if (pulse < -motor_pwm_period) pulse = -motor_pwm_period;
+        TimOCInitStructure.TIM_Pulse = -pulse;
+    }
+    else
+    {
+        GPIO_SetBits(GPIO_D, GPIO_Pin_0);
+        GPIO_ResetBits(GPIO_D, GPIO_Pin_0);
+        if (pulse > motor_pwm_period) pulse = motor_pwm_period;
+        TimOCInitStructure.TIM_Pulse = pulse;
+    }
+
+    TIM_OC1Init(TIM5, &TimOCInitStructure);
+    TIM_OC2Init(TIM5, &TimOCInitStructure);
+    TIM_OC3Init(TIM5, &TimOCInitStructure);
+    TIM_OC4Init(TIM5, &TimOCInitStructure); // channel 1-4 config
+    TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Enable);
+    TIM_OC2PreloadConfig(TIM5, TIM_OCPreload_Enable);
+    TIM_OC3PreloadConfig(TIM5, TIM_OCPreload_Enable);
+    TIM_OC4PreloadConfig(TIM5, TIM_OCPreload_Enable); // channel 1-4 config
+    TIM_ARRPreloadConfig(TIM5, ENABLE);
+    TIM_Cmd(TIM5, ENABLE);
+    TIM_CtrlPWMOutputs(TIM5, ENABLE);
+    TIM_Cmd(TIM5, ENABLE);
+    TIM_CtrlPWMOutputs(TIM5, ENABLE);
     return;
 }
